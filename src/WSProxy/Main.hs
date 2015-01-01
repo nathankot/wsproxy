ec679043ebc6104d8a0387fcf9cbce10c7abd749
Nathan Kot <me@nathankot.com>
2015-01-01 13:02:34 +0900

    Restructure + stubs for pullmessage.

--- src/WSProxy/Main.hs
+++ src/WSProxy/Main.hs
@@ -1,5 +1,3 @@
--- vim: foldmethod=marker
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module WSProxy.Main
@@ -9,8 +7,9 @@ module WSProxy.Main
 
 import Control.Applicative ((<$>))
 import Control.Concurrent (forkIO)
-import Control.Concurrent.MVar (newMVar, newEmptyMVar, readMVar)
+import Control.Concurrent.MVar (MVar, newMVar, newEmptyMVar, readMVar)
 import Control.Exception (finally)
+import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
@@ -31,7 +30,6 @@ getEnvWithDefault name defaultValue = do
 
 main :: IO ()
 main = do
-  -- Find the env
   port <- read <$> getEnvWithDefault "PORT" "3636" :: IO Int
   websocketPort <- read <$> getEnvWithDefault "WEBSOCKET_PORT" "9160" :: IO Int
   host <- getEnvWithDefault "HOST" "0.0.0.0"
@@ -42,10 +40,18 @@ application host port websocketPort = do
   -- Store state in MVar's
   state <- newMVar newClients
   messenger <- newEmptyMVar :: IO Messenger
-
+  -- This is the layer that passes messages from server to
+  -- client and vice-versa.
+  _ <- (forkIO $ listenToMessenger messenger)
   -- Fork a websockets server
   putStrLn $ "Websocket listening on port " ++ show websocketPort
-  _ <- forkIO $ WS.runServer host websocketPort $ \pending -> do
+  _ <- forkIO $ WS.runServer host websocketPort $ wsServer state messenger
+  -- Initialize scotty for our RESTFUL api
+  putStrLn $ "REST API listening on port " ++ show port
+  scotty port $ httpServer state messenger
+
+wsServer :: MVar Clients -> Messenger -> WS.PendingConnection -> IO ()
+wsServer state messenger pending = do
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
     msg <- WS.receiveData conn :: IO T.Text
@@ -54,26 +60,23 @@ application host port websocketPort = do
           c = (email, conn)
       in flip finally (disconnect state c) $ do
         _ <- connect state c
-        listenToMessenger messenger
+        -- take messages and send them to the server
+        forever $ do
+          m <- WS.receiveData conn :: IO T.Text
+          pullMessage messenger m [c]
     else
       WS.sendTextData conn $ T.pack "Bad use of protocol"
     return ()
 
-  -- Initialize scotty for our RESTFUL api
-  putStrLn $ "REST API listening on port " ++ show port
-  scotty port $ do
-
+httpServer :: MVar Clients -> Messenger -> ScottyM ()
+httpServer state messenger = do
     middleware logStdout
-
-    get "/ping" $ do
-      status status200
-      text "pong"
 
     post "/push" $ do
       clients <- liftIO $ readMVar state
       email <- param "email" `rescue` const next :: ActionM T.Text
       msg <- param "message" `rescue` const next :: ActionM T.Text
-      _ <- sendMessage messenger msg $ findAllByEmail email clients
+      _ <- pushMessage messenger msg $ findAllByEmail email clients
       status status200
       text "Acknowledged"
 
--- src/WSProxy/Messenger.hs
+++ src/WSProxy/Messenger.hs
@@ -2,7 +2,8 @@ module WSProxy.Messenger
 ( Messenger
 , Message (PushMessage, client, message)
 , listenToMessenger
-, sendMessage
+, pushMessage
+, pullMessage
 ) where
 
 import Control.Concurrent.MVar (takeMVar, putMVar, MVar)
@@ -17,6 +18,7 @@ import WSProxy.Types
 
 execute :: Message -> IO()
 execute (PushMessage { client = (_, conn), message = m }) = WS.sendTextData conn m
+execute (PullMessage { client = (email, _), message = m }) = return ()
 
 listenToMessenger :: Messenger -> IO ()
 listenToMessenger messenger = forever $ do
@@ -24,11 +26,14 @@ listenToMessenger messenger = forever $ do
   execute instruction
   return ()
 
-sendMessage :: MVar Message -> T.Text -> [Client] -> ActionM [()]
-sendMessage messenger m clients = do
+pushMessage :: Messenger -> T.Text -> Clients -> ActionM [()]
+pushMessage messenger m clients = do
     let messages = map (\c -> PushMessage { client = c, message = m }) clients
     let send = liftIO . putMVar messenger
     if null messages
     then fail "No clients to send to"
     else sequence [send a | a <- messages]
 
+pullMessage :: Messenger -> T.Text -> Clients -> IO ()
+pullMessage messenger m clients = do
+    return ()
