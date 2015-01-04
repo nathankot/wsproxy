ae861d12da49189976bd77f160d0ed61dcdf74f2
Nathan Kot <me@nathankot.com>
2015-01-05 02:11:12 +0900

    It compiles.

--- src/WSProxy/Main.hs
+++ src/WSProxy/Main.hs
@@ -16,6 +16,8 @@ import qualified Data.Text                  as T
 import Network.HTTP.Types                   (status200, status400)
 import System.Environment                   (lookupEnv)
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Handler.WebSockets
+import qualified Network.Wai.Handler.Warp   as W
 import Web.Scotty
 import qualified Network.WebSockets         as WS
 import WSProxy.Client
@@ -43,38 +45,19 @@ application e = do
     putStrLn "Starting wsproxy with environment >>"
     print e
     -- Store state in MVar's
-    state <- newMVar newClients
+    s <- newMVar newClients
     m <- newEmptyMVar :: IO Messenger
     -- This is the layer that passes messages from server to client and vice-versa.
-    _ <- forkIO $ listenToMessenger m
+    void $ forkIO $ listenToMessenger m
     -- Fork a websockets server
-    _ <- forkIO $ WS.runServer (host e) (websocketPort e)
-                $ wsServer m state (server e)
-    -- Initialize scotty for our RESTFUL api
-    -- Scotty will hold the main thead open
-    scotty (port e) $ httpServer state m
+    void $ forkIO $ WS.runServer (host e) (websocketPort e)
+                  $ wsServer m s (server e)
 
-wsServer :: Messenger -> MVar Clients -> Server -> WS.PendingConnection -> IO ()
-wsServer me s se p = do
-    conn <- WS.acceptRequest p
-    WS.forkPingThread conn 30
-    msg <- WS.receiveData conn :: IO T.Text
-    unless (isConnection msg) $ fail "Bad use of protocol"
-    WS.sendTextData conn ("Connection acknowledged" :: T.Text)
-    let email = T.drop (T.length connectPrefix) msg
-    let c = (email, conn)
-    void $ finally (connect s c
-           -- Start receiving messages.
-           >> forever (WS.receiveData conn
-           -- And forwarding them.
-           >>= \m -> pushMessage ServerMessage { messenger = me
-                                               , message = m, client = c
-                                               , recipientServer = se }))
-           (disconnect s c) -- Close connection on failure.
+    http <- scottyApp $ httpServer s m
+    W.run (port e) $ websocketsOr (WS.ConnectionOptions $ return ()) (wsServer m s (server e)) http
 
 httpServer :: MVar Clients -> Messenger -> ScottyM ()
 httpServer s m = do
-
     middleware logStdout
     get "/" $ status status200
 
@@ -91,3 +74,21 @@ httpServer s m = do
       status status400
       text "This endpoint requires an email and message"
 
+
+wsServer :: Messenger -> MVar Clients -> Server -> WS.ServerApp
+wsServer me s se p = do
+    conn <- WS.acceptRequest p
+    WS.forkPingThread conn 30
+    msg <- WS.receiveData conn :: IO T.Text
+    unless (isConnection msg) $ fail "Bad use of protocol"
+    WS.sendTextData conn ("Connection acknowledged" :: T.Text)
+    let email = T.drop (T.length connectPrefix) msg
+    let c = (email, conn)
+    void $ finally (connect s c
+           -- Start receiving messages.
+           >> forever (WS.receiveData conn
+           -- And forwarding them.
+           >>= \m -> pushMessage ServerMessage { messenger = me
+                                               , message = m, client = c
+                                               , recipientServer = se }))
+           (disconnect s c) -- Close connection on failure.
